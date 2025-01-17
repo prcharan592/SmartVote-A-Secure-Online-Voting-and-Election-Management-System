38ffc62065ab491b4e004b687e105f95888556d3
prcharan592 <149227007+prcharan592@users.noreply.github.com>
2025-01-17 11:22:45 +0530

    Update footer.php

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -2,5 +2,4 @@
     <div class="pull-right hidden-xs">
       <b>All rights reserved</b>
     </div>
-    <strong>Copyright &copy; 2021 <a href="https://www.campcodes.com">CampCodes</a></strong>
 </footer>
